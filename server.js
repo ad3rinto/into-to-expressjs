a40e2fdf85177b7ba9e9ee15221d3124cd7c79fb
ad3rinto <aad3rinto@gmail.com>
2022-08-06 15:14:49 +0100

    update

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", function(req, res){
-    res.send("<h1>Hello</h1>")
+    res.send("<h1>Hello, World!</h1>")
 
 })
+
+
+app.get("/about", function(req, res){
+    res.send("My name is Katanga")
+})
+
 app.listen(3000, function(){
     console.log(`server started on port ${PORT}`)
 });
